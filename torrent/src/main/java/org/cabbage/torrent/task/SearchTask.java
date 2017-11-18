298cae0bad5ae84077831a21ee5bd84bec41b5b6
endend20000 <end.2000@163.com>
2017-11-18 22:10:48 +0800

    增加多綫程讀

---
+++ torrent/src/main/java/org/cabbage/torrent/task/SearchTask.java
@@ -0,0 +1,21 @@
+package org.cabbage.torrent.task;
+
+import java.util.concurrent.Callable;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class SearchTask  implements Callable<Document>{
+    private String url;  
+    public SearchTask(String url){  
+        this.url  = url;  
+    }  
+      
+    public Document call(){  
+ 		try{
+ 			return Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").timeout(1000*15).get();
+ 		}catch(Exception ex){
+ 			return null;
+ 		}
+    }  
+}
